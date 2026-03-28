36df65aa131aaf6e14ce7d5c7da936e99c585c9d
Ivan Dubrovin <books.booksovich@yandex.com>
2026-03-28 11:47:32 +0300

    Add a docstring for the entire module

--- my_ext.cpp
+++ my_ext.cpp
@@ -5,6 +5,8 @@ namespace nb = nanobind;
 int add(int a, int b = 1) { return a + b; }
 
 NB_MODULE(my_ext, m) {
+    m.doc() = "A simple example python extension";
+
     m.def("add", &add, nb::arg("a"), nb::arg("b") = 1,
           "This function adds two numbers and increments if only one is provided.");
 
