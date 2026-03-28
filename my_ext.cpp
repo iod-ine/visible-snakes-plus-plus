ae5ee9ab9854d9179b00d97f2426c1072881196e
Ivan Dubrovin <books.booksovich@yandex.com>
2026-03-28 11:45:52 +0300

    Add a value export

--- my_ext.cpp
+++ my_ext.cpp
@@ -7,4 +7,6 @@ int add(int a, int b = 1) { return a + b; }
 NB_MODULE(my_ext, m) {
     m.def("add", &add, nb::arg("a"), nb::arg("b") = 1,
           "This function adds two numbers and increments if only one is provided.");
+
+    m.attr("the_answer") = 42;
 }
