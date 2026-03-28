f759e71037db744a0d04381414ac3e9d8a65e3ca
Ivan Dubrovin <books.booksovich@yandex.com>
2026-03-28 12:13:19 +0300

    Add usage examples in main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from visible-snakes-plus-plus!")
+import sys
 
 
 if __name__ == "__main__":
-    main()
+    sys.path.append("build")
+    import my_ext
+
+    print(my_ext.add(423, 123))
+    print(my_ext.the_answer)
+
+    rex = my_ext.Dog("Rex")
+    print(rex)
+    print(rex.bark())
